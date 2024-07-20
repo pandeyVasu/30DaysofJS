16f9ff837669318d1f0b743aacfb6c755ce7f7a3
Vasundhara Pandey <145595184+pandeyVasu@users.noreply.github.com>
2024-07-21 01:42:44 +0530

    Add files via upload

---
+++ 30daysofJS/day1/1.js
@@ -0,0 +1,38 @@
+// declaring variable using var
+var number = 10;
+console.log(number);
+
+// // declaration of variable using let
+let string = "vasu";
+console.log(string);
+
+// declaration of variable using const 
+const value = false;
+console.log(value);
+
+// data type
+let age = 10;
+console.log(typeof(age));
+
+let str = "vasuqt";
+console.log(typeof(str));
+
+let type = true;
+console.log(typeof(type));
+
+let arr = [0,1,2,3];
+console.log(typeof(arr));
+
+// redeclaration
+
+let valueofx = 10;
+valueofx = 20;
+console.log(valueofx);
+
+
+// reassigning of variable using const
+
+const mathsmarks = 40;
+console.log(mathsmarks);
+mathsmarks = 45;
+console.log(mathsmarks);
---
+++ 30daysofJS/day2/2.js
@@ -0,0 +1,41 @@
+// add two number
+
+let a = 10;
+let b = 20;
+c = a+b;
+console.log(c)
+
+// WAP  to substract
+let A = 20;
+let B = 10;
+C = A-B;
+console.log(C);
+
+// multiply two number
+let P= 10;
+let Q = 20;
+R = P*Q;
+console.log(R);
+
+// divide num
+let m = 20;
+let n = 2;
+q = m/n;
+console.log(q)
+
+// remainder 
+let M = 15;
+let N  = 2;
+Q = M%N
+console.log(Q);
+
+// assignment operartor 
+let d = 10;
+d+=5;
+console.log(d);
+let f = 15;
+f-=5;
+console.log(f);
+
+// comparison operators 
+
